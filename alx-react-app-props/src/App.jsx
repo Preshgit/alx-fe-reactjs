81567853af21abe0d74b37fee0a3c944324c8054
preshgit <preciousdesk10@gmail.com>
2025-02-23 23:58:41 +0100

    new commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,17 +7,15 @@ import MainContent from "./components/MainContent";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import { UserContext } from "./components/UserContext";
+import ProfilePage from "./components/ProfilePage";
 function App() {
-  const [count, setCount] = useState(0);
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <>
-      <Header />
-      <WelcomeMessage />
-      <MainContent />
-      <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-    </>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
