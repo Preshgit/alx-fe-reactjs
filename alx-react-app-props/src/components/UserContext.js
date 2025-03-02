448016d3f59017c8b01673eb928a5f691ab7befa
preshgit <preciousdesk10@gmail.com>
2025-03-02 20:42:44 +0100

    edited

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,6 +1,6 @@
+// src/components/UserContext.js
 import React from "react";
 
-// Initialize context using React.createContext() as per instructions
 const UserContext = React.createContext(null);
-// Export the context as default export
+
 export default UserContext;
