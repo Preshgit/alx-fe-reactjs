3d19760c1c76ca86ccc852828566c5812abef26c
preshgit <preciousdesk10@gmail.com>
2025-03-02 20:31:03 +0100

    updated userContext.js

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -5,6 +5,3 @@ const UserContext = createContext(null);
 
 // Export the context as default export
 export default UserContext;
-
-// You can also provide a named export if needed
-export { UserContext };
