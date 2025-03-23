ecea80443ad94e859f6d68a91c2f30f434fe62b1
preshgit <preciousdesk10@gmail.com>
2025-03-23 23:56:27 +0100

    feature: create REcipeDetail component and data.json file & installed react-router-dom

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import HomePage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
 import { BrowserRouter } from "react-router";
-import { BrowserRouter as Router, Route, Routes } from "react-router";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 function App() {
   return (
--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router";
+import { useParams, Link } from "react-router-dom";
 import { Clock, Users, ChevronLeft, Star } from "lucide-react";
 import recipeData from "../data.json";
 
