aea182d15f51818f5e5c0818060fd56b2055fae5
preshgit <preciousdesk10@gmail.com>
2025-03-23 22:59:01 +0100

    Remove recipe-sharing-platform from repository history

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,23 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+// module.exports = {
+//   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+//   darkMode: false,
+//   theme: { extend: {} },
+//   variants: { extend: {} },
+//   plugins: [],
+// };
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
