433f5e6e3a10cdba61487527a34450c360d74baa
preshgit <preciousdesk10@gmail.com>
2025-03-24 04:08:38 +0100

    setup: installed and initiallized tailwindcss

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  corePlugins: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  variants: {},
+  darkmode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
